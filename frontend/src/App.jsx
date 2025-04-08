b6078a685c220fdf00d736d736adaec8318ee505
piyushrajpurohit17 <piyushrajpurohit1704@gmail.com>
2025-04-08 17:20:53 +0530

    Piyush changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Error from './Components/ERROR/Error'
+import LandingPage from './Components/LandingPage/LandingPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage/LoginPage';
 
 function App() {
 
   return (
     <>
-      <Error/>
+      {/* <Error/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
