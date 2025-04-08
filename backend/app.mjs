dc583cdeb96b1c676ab89da5fbad4c4afeac886b
Krishna Kadukar <krishnakadukar.skillolite@gmail.com>
2025-04-08 22:21:50 +0530

    auth complete

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.mjs";
 
 import authRoutes from "./routes/authRoutes.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
-import { registerUser } from "./controllers/userController.mjs";
 
 dotenv.config(); 
 
@@ -14,10 +14,11 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({ origin: "*", credentials: true }));
 
 app.use("/api/auth",authRoutes);
-app.use("/api/register", registerUser);
+app.use("/api/register", userRoutes);
 
 
 export default app;
--- backend/controllers/authController.mjs
+++ backend/controllers/authController.mjs
@@ -1,18 +1,27 @@
 import User from "../models/user.mjs";
+import generateToken from "../utils/generateToken.mjs";
 
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
         const user = await User.findOne({ email });
-
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        if (user.password !== password) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
+        if (user.password !== password ){
+            return res.status(401).json({ message: "Invalid credentials" }); 
+        } 
+
+        const token = generateToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
 
         res.status(200).json({
             message: "Login successful",
@@ -21,9 +30,10 @@ export const loginUser = async (req, res) => {
                 name: user.name,
                 role: user.role,
                 email: user.email
-            }
+            },
+            token
         });
     } catch (err) {
         res.status(500).json({ message: "Server error", error: err.message });
     }
-};
+}
