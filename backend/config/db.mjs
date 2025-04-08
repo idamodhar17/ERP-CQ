e4b471f0257d9f67370b234f66c8c0c8dc0fa267
Damodhar Zanwar <148037513+idamodhar17@users.noreply.github.com>
2025-04-08 17:09:11 +0530

    Updated db.js

--- backend/config/db.mjs
+++ backend/config/db.mjs
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("Connected to MongoDB");
   } catch (err) {
