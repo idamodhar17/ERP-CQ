d6064741050ef723c23524927629174cfafe81b3
SrujalYandagoudar <131784602+SrujalYandagoudar@users.noreply.github.com>
2025-04-08 14:01:03 +0530

    erroe 404

---
+++ frontend/src/Components/ERROR/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div className='error h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-bold text-9xl'>404</h1>
+      <p className='font-bold text-4xl'>Page Not Found</p>
+    </div>
+  )
+}
