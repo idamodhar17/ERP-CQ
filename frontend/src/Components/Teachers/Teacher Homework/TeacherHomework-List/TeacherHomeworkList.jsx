53364c91aaf0ea6a3532d94e49ee2182e825cd1c
piyushrajpurohit17 <piyushrajpurohit1704@gmail.com>
2025-04-10 12:30:00 +0530

    piyush changes

--- frontend/src/Components/Teachers/Teacher Homework/TeacherHomework-List/TeacherHomeworkList.jsx
+++ frontend/src/Components/Teachers/Teacher Homework/TeacherHomework-List/TeacherHomeworkList.jsx
@@ -1,13 +1,16 @@
-import { Search  } from "lucide-react";
-import { Copy , FileText , Sheet } from 'lucide-react';
+import { Copy, FileText, Sheet , Search } from 'lucide-react';
+import { useState } from "react";
 
 function TeacherHomeworkList() {
+
+    const [show, setShow] = useState(false)
+
     return (
         <div className="TeacherHomeworkList-Page">
-           <div className="flex justify-between">
-             <h1 className="p-2">Homework List</h1>
-             <button className=" text-white bg-blue-500 rounded-lg px-3 py-1">+ Add Homework</button>
-             </div>
+            <div className="flex justify-between">
+                <h1 className="p-2">Homework List</h1>
+                <button className=" text-white bg-blue-500 rounded-lg px-3 py-1">+ Add Homework</button>
+            </div>
             <div className="grid grid-cols-3 items-center gap-5 text-gray-500 ">
                 <div className="flex flex-col gap-2 text-gray-500 ">
                     <label className='text-gray-500'>CLASS</label>
@@ -42,51 +45,56 @@ function TeacherHomeworkList() {
                     </select>
                 </div>
 
-                
+
 
             </div>
             <div className="flex justify-end items-center  mt-4">
-                    <button className='flex  items-center gap-4 text-white px-8 py-1 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 '><Search/> SEARCH</button>
-                </div>
-
-                <div className=" mt-10 shadow-lg p-4">
-              <div className="grid grid-cols-3 items-center">
-                <div className="">
-                  <h1 className="">Homework List</h1>
-                </div>
-
-                <div className="flex items-center gap-2 ">
-                    <Search/>
-                    <input type="text" className='border-b-2 border-b-blue-200 outline-0 px-4 py-1 ' placeholder='Search...' />
-                </div>
-
-                <div className="flex items-center justify-self-center gap-6 border-2 border-blue-300 px-10 py-2 rounded-2xl">
-                <button type="button" className='text-blue-500 hover:text-blue-700 '><Copy/></button>
-                      <button type="button" className='text-blue-500  hover:text-blue-700 border-x-2 border-x-blue-400 px-3'><Sheet/></button>
-                      <button type="button" className='text-blue-500 hover:text-blue-700 '><FileText/></button>
-                </div>
-              </div>
-
-              <div className="w-full mt-6">
-                <table className='w-full'>
-                  <thead className='bg-gray-100 py-2'>
-                      <td className="p-2">SL</td>
-                      <td className="">Class</td>
-                      <td className="">Section</td>
-                      <td className="">Subject</td>
-                      <td className="">Marks</td>
-                      <td className="">Homework Date</td>
-                      <td className="">Submition Date</td>
-                      <td className="">Evalution Date</td>
-                      <td className="">Created By</td>
-                    
-                  </thead>
-                
-                </table>
-              </div>
+                <button className='flex  items-center gap-4 text-white px-8 py-1 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 '
+                    onClick={() => { setShow(!show) }}
+                ><Search /> SEARCH</button>
             </div>
+            {
+  show && (
+            <div className=" mt-10 shadow-lg p-4">
+                <div className="grid grid-cols-3 items-center">
+                    <div className="">
+                        <h1 className="">Homework List</h1>
+                    </div>
+
+                    <div className="flex items-center gap-2 ">
+                        <Search />
+                        <input type="text" className='border-b-2 border-b-blue-200 outline-0 px-4 py-1 ' placeholder='Search...' />
+                    </div>
+
+                    <div className="flex items-center justify-self-center gap-6 border-2 border-blue-300 px-10 py-2 rounded-2xl">
+                        <button type="button" className='text-blue-500 hover:text-blue-700 '><Copy /></button>
+                        <button type="button" className='text-blue-500  hover:text-blue-700 border-x-2 border-x-blue-400 px-3'><Sheet /></button>
+                        <button type="button" className='text-blue-500 hover:text-blue-700 '><FileText /></button>
+                    </div>
+                </div>
+
+                <div className="w-full mt-6">
+                    <table className='w-full'>
+                        <thead className='bg-gray-100 py-2'>
+                            <td className="p-2">SL</td>
+                            <td className="">Class</td>
+                            <td className="">Section</td>
+                            <td className="">Subject</td>
+                            <td className="">Marks</td>
+                            <td className="">Homework Date</td>
+                            <td className="">Submition Date</td>
+                            <td className="">Evalution Date</td>
+                            <td className="">Created By</td>
+
+                        </thead>
+
+                    </table>
+                </div>
+            </div>
+                ) 
+            }
         </div>
-    );  
+    );
 }
 
 export default TeacherHomeworkList;
---
+++ frontend/src/Resources/TopicResources/TopicResources.jsx
@@ -0,0 +1,134 @@
+import '../TopicResources/TopicResources.css'
+import { Copy, FileText, Sheet, Search } from 'lucide-react';
+
+function TopicResources() {
+    return (
+        <div className="TopicResources-Page">
+            <h1>Add Lesson</h1>
+            < div className="TopicResources-MainPage">
+                <div className="TopicResources-left">
+                    <h1>Add Lesson</h1>
+                    <div className="TopicResources-class">
+                        <label>Class</label>
+                        <select>
+                            <option>Select Class</option>
+                            <option>Class 1</option>
+                            <option>Class 2</option>
+                            <option>Class 3</option>
+                            <option>Class 4</option>
+                        </select>
+                    </div>
+                    <div className="TopicResources-section">
+                        <label>Section</label>
+                        <select>
+                            <option>Select Section</option>
+                            <option>Section 1</option>
+                            <option>Section 2</option>
+                            <option>Section 3</option>
+                            <option>Section 4</option>
+                        </select>
+                    </div>
+                    <div className="TopicResources-subject">
+                        <label>Subject</label>
+                        <select>
+                            <option>Select Subject</option>
+                            <option>Subject 1</option>
+                            <option>Subject 2</option>
+                            <option>Subject 3</option>
+                            <option>Subject 4</option>
+                        </select>
+                    </div>
+                    <div className="TopicResources-title">
+                        <label>Add Lesson Name</label>
+                        <input type="text" placeholder="Title" />
+                    </div>
+
+
+                    <div className='TopicResources-btn'>
+                        <button>Save lesson</button>
+                    </div>
+                </div>
+                <div className="ContentResources-right">
+                    <div className=" mt-10 shadow-lg p-4">
+                        <div className="grid grid-cols-3 items-center">
+                            <div className="">
+                                <h1 className="">Add Lesson</h1>
+                            </div>
+
+                            <div className="flex items-center gap-2 ">
+                                <Search />
+                                <input type="text" className='border-b-2 border-b-blue-200 outline-0 px-4 py-1 ' placeholder='Search...' />
+                            </div>
+
+                            <div className="flex items-center justify-self-center gap-6 border-2 border-blue-300 px-10 py-2 rounded-2xl">
+                                <button type="button" className='text-blue-500 hover:text-blue-700 '><Copy /></button>
+                                <button type="button" className='text-blue-500  hover:text-blue-700 border-x-2 border-x-blue-400 px-3'><Sheet /></button>
+                                <button type="button" className='text-blue-500 hover:text-blue-700 '><FileText /></button>
+                            </div>
+                        </div>
+
+                        <div className="w-full mt-6">
+                            <table className='w-full'>
+                                <thead className='bg-gray-100 py-2'>
+                                    <td className="p-2">SL</td>
+                                    <td className="">Class</td>
+                                    <td className="">Section</td>
+                                    <td className="">Subject</td>
+                                    <td className="">Lesson</td>     
+                                    <td className="">Action</td>
+
+                                </thead>
+                                <tbody className='text-gray-500'>
+                                    <tr className="border-b-2 border-b-gray-200">
+                                        <td className="p-2">1</td>
+                                        <td className="">Class 1</td>
+                                        <td className="">A</td>
+                                        <td className="">Subject 1</td>
+                                        <td className="">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
+                                        <td className=""><button>Select</button></td>
+                                    </tr>
+                                    <tr className="border-b-2 border-b-gray-200">
+                                        <td className="p-2">1</td>
+                                        <td className="">Class 1</td>
+                                        <td className="">A</td>
+                                        <td className="">Subject 1</td>
+                                        <td className="">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
+                                        <td className=""><button>Select</button></td>
+                                    </tr>
+                                    <tr className="border-b-2 border-b-gray-200">
+                                        <td className="p-2">1</td>
+                                        <td className="">Class 1</td>
+                                        <td className="">A</td>
+                                        <td className="">Subject 1</td>
+                                        <td className="">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
+                                        <td className=""><button>Select</button></td>
+                                    </tr>
+                                    <tr className="border-b-2 border-b-gray-200">
+                                        <td className="p-2">1</td>
+                                        <td className="">Class 1</td>
+                                        <td className="">A</td>
+                                        <td className="">Subject 1</td>
+                                        <td className="">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
+                                        <td className=""><button>Select</button></td>
+                                    </tr>
+                                    <tr className="border-b-2 border-b-gray-200">
+                                        <td className="p-2">1</td>
+                                        <td className="">Class 1</td>
+                                        <td className="">A</td>
+                                        <td className="">Subject 1</td>
+                                        <td className="">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
+                                        <td className=""><button>Select</button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default TopicResources;
