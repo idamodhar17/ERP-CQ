86a5884eac60228a4aa9127151e43a6456663370
Krishna Kadukar <krishnakadukar.skillolite@gmail.com>
2025-04-10 14:42:17 +0530

    Admission and subject

--- backend/app.mjs
+++ backend/app.mjs
@@ -7,13 +7,10 @@ import connectDB from "./config/db.mjs";
 import resourceRoutes from './routes/resourceRoutes.mjs'
 import authRoutes from "./routes/authRoutes.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
-<<<<<<< Updated upstream
 import teacherRoutes from "./routes/teacherRoutes.mjs"
-=======
 import studentRoutes from './routes/studentRoutes.mjs';
 import classRoutes from "./routes/classRoutes.mjs";
 import subjectRoutes from "./routes/subjectRoutes.mjs";
->>>>>>> Stashed changes
 
 dotenv.config(); 
 
@@ -27,17 +24,11 @@ app.use(cors({ origin: "*", credentials: true }));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/register", userRoutes);
-
 app.use('/api/resources', resourceRoutes);
-<<<<<<< Updated upstream
 app.use('/api/teacher', teacherRoutes);
-=======
-
-
 app.use("/api/student", studentRoutes);
 app.use('/api/classes', classRoutes);
 app.use('/api/subjects', subjectRoutes);
->>>>>>> Stashed changes
 
 export default app;
 
